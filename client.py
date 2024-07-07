fda4d4a1e15fee8246f1369659580b85bb973cd9
jch618 <you@example.com>
2024-07-08 06:03:47 +0900

    add function: git all

--- client.py
+++ client.py
@@ -4,42 +4,73 @@ import sys
 ip = "127.0.0.1"
 port = 9999
 
+# request message를 만들어 return하는 함수
 def makeRequestMessage(userInput):
+    # 잘못된 input 처리
+    if (len(userInput.split()) != 2):
+        return -1
+    # get a -> method = get, name = a
     method, name = userInput.split()
     method = method.upper()
     responseBody = ""
     if (method == "GET"):
-        pass
+        # 모든 data를 요청할 경우 path를 "/"로 설정하기 위해 name을 ""로 설정한다
+        if (name.upper() == "ALL"):
+            name = ""
     elif (method == "HEAD"):
         pass
     elif (method == "POST" or method == "PUT"):
+        # 추가/수정할 데이터 입력을 요청한다
         print("Enter information: [type] [age] [leg]")
         userInput = sys.stdin.readline().strip().split()
+        # 잘못된 input 처리
+        if (len(userInput) != 3):
+            return -1
+        # reponse body는 application/json 형태로 구성한다
         responseBody = str({ name : { "type": userInput[0], "age": userInput[1], "leg": userInput[2]}})
 
-    directory = "/" + name
+    # request message를 구성하여 return한다
+    path = "/" + name
     contentType = "application/json"
     contentLength = len(responseBody)
-    httpHeader = f"{method} {directory} HTTP/1.1\r\nContent-Type: {contentType}\r\nContent-Length: {contentLength}\r\n\n"
+    httpHeader = f"{method} {path} HTTP/1.1\r\nContent-Type: {contentType}\r\nContent-Length: {contentLength}\r\n\n"
     return httpHeader + responseBody
     
-
+# IPv4 주소를 사용하고 TCP 통신을 한다.
 clientSocket = socket(AF_INET, SOCK_STREAM)
+# server의 ip, port 주소로 접속한다
 clientSocket.connect((ip, port))
-print("connect ok")
 # httpHeader = f"GET / HTTP/1.1\r\nContent-Type: {}\r\nContent-Length: {}\r\n\n"
 
-# send request
+# request response를 주고 받는 while문
 while True:
 
+    # 사용자로부터 input을 받는다
     print("Enter: [method] [objectName]")
     userInput = sys.stdin.readline().strip()
+
+    # q를 입력할 경우 while문 실행 종료
     if (userInput == "q"):
         break
-    clientSocket.send(makeRequestMessage(userInput).encode("utf-8"))
+    
+    # request message를 생성한다
+    requestMessage = makeRequestMessage(userInput)
+    # 잘못된 input 처리
+    if (requestMessage == -1):
+        print("Wrong Input: try again")
+        continue
+    
+    # request message를 server에 전송한다
+    clientSocket.send(requestMessage.encode("utf-8"))
 
+    # server로부터 response message를 받는다
     data = clientSocket.recv(1024)
     responseMessage = data.decode("utf-8")
-    print("data:\n" + responseMessage)
+    
+    print("----------RESPONSE------------\n")
+    print(responseMessage)
+    print("------------------------------\n")
 
+
+# clinet를 종료한다
 clientSocket.close()
--- server.py
+++ server.py
@@ -1,72 +1,118 @@
 from socket import *
-import email
-import sys
 
+#host = "192.168.0.15"
 host = "127.0.0.1"
 port = 9999
 
+# data table
 table = {
     "Doldol" : { "type" : "chicken", "age" : 1, "leg" : 2 },
     "Baduk" : { "type" : "dog", "age" : 3, "leg" : 4 },
     "Yaong" : { "type" : "cat", "age" : 2, "leg" : 4 }
 }
 
-# httpHeader = f"GET / HTTP/1.1\r\nContent-Type: {}\r\nContent-Length: {}\r\n\n"
-# responseMessage = f"HTTP/1.1 {status} {statusMessage}\r\nContent-Type: {}\r\nContent-Length: {}\r\n\n" 
+# request message format: f"GET / HTTP/1.1\r\nContent-Type: {}\r\nContent-Length: {}\r\n\n"
+# response message format = f"HTTP/1.1 {status} {statusMessage}\r\nContent-Type: {}\r\nContent-Length: {}\r\n\n" 
+
+# http 상태 코드가 500일 때 처리
+def handleStatus500():
+    status = 500
+    statusMessage = "Internal Server Error"
+    return (status, statusMessage)
+
+# http 상태 코드가 204일 때 처리
+def handleStatus204():
+    status = 204
+    statusMessage = "No Content"
+    return (status, statusMessage)
+
+def handleStatus201():
+    status = 201
+    statusMessage = "Created"
+    return (status, statusMessage)
+
+# request message의 요청에 따라 알맞은 response message를 생성해 return하는 함수
 def makeResponseMessage(message):
+    # request message에서 Content type을 application/json으로 설정하였기 때문에
+    # body는 { "a" : "value1", "b" : "value2" } 꼴이다
     index = message.find("{")
     requestBody = message[index:]
-    firstHeader, headers = message[:index].split("\r\n", 1)
-    message = email.message_from_string(headers)
-    headers = dict(message.items())
 
-    (method, directory, _) = firstHeader.split()
+    # firstHeader는 header의 첫 line이다 ex) GET / HTTP/1.1
+    firstHeader = message[:index].split("\r\n", 1)[0]
+
+    (method, path, _) = firstHeader.split()
     status = 200
     statusMessage = "OK"
     responseBody = ""
+    key = path[1:]
+    # method에 따라 알맞은 reponse message를 구성한다
     if (method == "GET"):
-        responseBody = str(table[directory[1:]])
+        # path에서 '/'를 제외한 값을 key로 사용 
+        # path가 /일 경우 table의 모든 data를 전송한다
+        if (path == "/"):
+            responseBody = str(table)
+        # 만약 key가 data table에 없을 경우 No Content 처리한다
+        elif (key not in table):
+            status, statusMessage = handleStatus204()
+        else:
+            responseBody = str({ key : table[key] })
     elif (method == "HEAD"):
-        responseBody = ""
+        key = path[1:]
+        # 만약 key가 data table에 없을 경우 No Content 처리한다
+        if (key not in table):
+            status, statusMessage = handleStatus204()
     elif (method == "POST"):
         table.update(eval(requestBody))
-        responseBody = str(table[directory[1:]])
-        status = 201
-        statusMessage = "Created"
+        responseBody = str({ key : table[key]})
+        status, statusMessage = handleStatus201()
     elif (method == "PUT"):
-        table.update(eval(requestBody))
-        responseBody = str(table[directory[1:]])
+        newDict = eval(requestBody)
+        # 없던 key를 추가할 경우 state를 201 Created로 설정한다 아니라면 200 OK이다.
+        if (key not in table):
+            status, statusMessage = handleStatus201()
+        table.update(newDict)
+        responseBody = str({ key : table[key] })
+
+    # Content-Length
     bodySize = len(responseBody)
+
+    # response message를 구성한다
     responseMessage = f"HTTP/1.1 {status} {statusMessage}\r\nContent-Type: application/json\r\nContent-Length: {bodySize}\r\n\n"
     responseMessage += responseBody
 
-    print("responseMessage: " + responseMessage)
     return responseMessage
 
 
-#init
 
+# IPv4 주소를 지정하고, TCP를 사용한다
 serverSocket = socket(AF_INET, SOCK_STREAM)
+# server를 설정한 ip와 port binding 한다
 serverSocket.bind((host, port))
+# clinet의 요청을 대기한다
 serverSocket.listen(1)
 print("Server: listening...")
 
+# client가 접속됐을 경우 새로운 socket과 client의 주소를 return값으로 받는다.
 connectionSocket, clientAddress = serverSocket.accept()
+
+# request-response를 주고 받는 while문
 while True:
 
-    print(f"Connected from {str(clientAddress)}")
+    # client로부터 request를 받는다
     data = connectionSocket.recv(1024)
     requestMessage = data.decode("utf-8")
-    print(f"Received message: {requestMessage}")
 
-    connectionSocket.send(makeResponseMessage(requestMessage).encode("utf-8"))
+    print("-----------REQUEST------------\n")
+    print(requestMessage)
+    print("------------------------------\n")
 
-    serverSocket.close()
+    # request 메시지를 해석하고 response를 보낸다
+    try:
+        connectionSocket.send(makeResponseMessage(requestMessage).encode("utf-8"))
+    except:
+        break
 
 
-
-
-# 1. bind
-# 2. listen
-# while 문
-# accept
+# server socket을 종료한다
+serverSocket.close()
